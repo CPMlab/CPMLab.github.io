d3ea58a8978913ec1321fef5553b675ab0c5450c
mkhwang <lanoon28@gmail.com>
2026-07-29 16:12:45 +0900

    논문 수정

--- js/publications-data.js
+++ js/publications-data.js
@@ -1,8 +1,6 @@
 /** Publication URLs in same order as .paper-item elements on publications.html */
 window.PUBLICATION_URLS = [
   'https://febs.onlinelibrary.wiley.com/doi/10.1002/1878-0261.70302',
-  'https://aacrjournals.org/cancerres/article/86/7_Supplement/3960/779689',
-  'https://aacrjournals.org/cancerres/article/86/7_Supplement/3531/781082',
   'https://pubmed.ncbi.nlm.nih.gov/41460723/',
   'https://doi.org/10.1016/j.mocell.2025.100272',
   'https://pubmed.ncbi.nlm.nih.gov/40554806/',
